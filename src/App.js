67fc6931a3f0bf193a670dd7b169665b1ee18415
Niranjan <niranjan32331@gmail.com>
2019-07-23 18:58:33 +0530

    changed text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          HOsdfsdlaaaaaaaaaaaaAAAAAAAAAAAAaaaaaaaa...
+          Another change.. as change is the only constant.
         </p>
         <a
           className="App-link"
